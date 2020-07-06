c800207c51982b8b52837c1497b14e3ac4f1248e
Ricardo Ramírez <rickyrb@gmail.com>
2020-07-06 16:07:33 -0500

    Versión inicial

---
+++ getAll.py
@@ -0,0 +1,60 @@
+import json
+import boto3
+import decimal
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("DemoServerless")
+
+headers = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': '*',
+	'Content-Type': 'application/json'
+}
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if abs(o) % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+def lambda_handler(event, context):
+	print(event)
+	
+	try:
+		response = table.query(
+			KeyConditionExpression = Key("hk").eq("EMPLEADO")
+		)
+		print(response)
+
+	except ClientError as e:
+		return {
+			"statusCode": 500,
+			"headers": headers,
+			"body": json.dumps({"error": e.response['Error']['Message']})
+		}
+
+	else:
+		if "Items" not in response or not response["Items"]:
+			return {
+				"statusCode": 404,
+				"headers": headers,
+				"body": json.dumps({"error": "No existe ningún registros"})
+			}
+
+		registros = response['Items']
+		for registro in registros:
+			registro["codigo"] = registro["sk"]
+			del registro["hk"]
+			del registro["sk"]
+			del registro["busqueda"]
+
+		return {
+			"statusCode": 200,
+			"headers": headers,
+			"body": json.dumps(registros, cls=DecimalEncoder, ensure_ascii=False)
+		}
